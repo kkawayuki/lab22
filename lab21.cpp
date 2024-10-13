4088d803393df32c25b52d981dbf8dfccae8ef9c
kkawayuki <kyukiokawa@gmail.com>
2024-10-12 21:13:47 -0700

    Commit 2

--- lab21.cpp
+++ lab21.cpp
@@ -3,66 +3,81 @@ using namespace std;
 
 const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
 
-class DoublyLinkedList {
+class DoublyLinkedList
+{
 private:
-    struct Node {
+    struct Node
+    {
         int data;
-        Node* prev;
-        Node* next;
-        Node(int val, Node* p = nullptr, Node* n = nullptr) {
-            data = val; 
+        Node *prev;
+        Node *next;
+        Node(int val, Node *p = nullptr, Node *n = nullptr)
+        {
+            data = val;
             prev = p;
             next = n;
         }
     };
 
-    Node* head;
-    Node* tail;
+    Node *head;
+    Node *tail;
 
 public:
     // constructor
-    DoublyLinkedList() { head = nullptr; tail = nullptr; }
+    DoublyLinkedList()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
 
-    //methods 
-    void push_back(int value) {
-        Node* newNode = new Node(value);
-        if (!tail)  // if there's no tail, the list is empty
+    // methods
+    void push_back(int value)
+    {
+        Node *newNode = new Node(value);
+        if (!tail) // if there's no tail, the list is empty
             head = tail = newNode;
-        else {
+        else
+        {
             tail->next = newNode;
             newNode->prev = tail;
             tail = newNode;
         }
     }
 
-    void push_front(int value) {
-        Node* newNode = new Node(value);
-        if (!head)  // if there's no head, the list is empty
+    void push_front(int value)
+    {
+        Node *newNode = new Node(value);
+        if (!head) // if there's no head, the list is empty
             head = tail = newNode;
-        else {
+        else
+        {
             newNode->next = head;
             head->prev = newNode;
             head = newNode;
         }
     }
 
-    void insert_after(int value, int position) {
-        if (position < 0) {
+    void insert_after(int value, int position)
+    {
+        if (position < 0)
+        {
             cout << "Position must be >= 0." << endl;
             return;
         }
 
-        Node* newNode = new Node(value);
-        if (!head) {
+        Node *newNode = new Node(value);
+        if (!head)
+        {
             head = tail = newNode;
             return;
         }
 
-        Node* temp = head;
+        Node *temp = head;
         for (int i = 0; i < position && temp; ++i)
             temp = temp->next;
 
-        if (!temp) {
+        if (!temp)
+        {
             cout << "Position exceeds list size. Node not inserted.\n";
             delete newNode;
             return;
@@ -77,80 +92,142 @@ public:
         temp->next = newNode;
     }
 
-    void delete_pos(int value) { //NOTE: assumes user knows that index starts at zero
-        if (!head) return; // Empty list
+    void delete_pos(int value)
+    { // NOTE: assumes user knows that index starts at zero
+        if (!head)
+            return; // Empty list
 
-        Node* temp = head;
+        Node *temp = head;
 
-        for(int i = 0; temp, i < value; i++)  // traverses through linked list from start if valid, based on value
-            temp = temp->next; //traverse until found
-        
-        if (!temp) return; // Value not found
+        for (int i = 0; temp, i < value; i++) // traverses through linked list from start if valid, based on value
+            temp = temp->next;                // traverse until found
 
-        //below code assumes that now temp points to the node to delete
-        if (temp->prev) {
+        if (!temp)
+            return; // Value not found
+
+        // below code assumes that now temp points to the node to delete
+        if (temp->prev)
+        {
             temp->prev->next = temp->next;
-        } else {
+        }
+        else
+        {
             head = temp->next; // Deleting the head
         }
 
-        if (temp->next) {
+        if (temp->next)
+        {
             temp->next->prev = temp->prev;
-        } else {
+        }
+        else
+        {
             tail = temp->prev; // Deleting the tail
         }
 
         delete temp;
     }
 
-    void delete_node(int value) {
-        if (!head) return; // Empty list
+    void pop_front()
+    { 
+        if (!head)
+            return; // Empty list
 
-        Node* temp = head;
+        Node *temp = head;
+
+        if (!temp)
+            return; // head not found
+
+        
+        head = temp->next; // Deleting the head
+        temp->next->prev = temp->prev;
+        
+        delete temp;
+    }
+
+    void pop_(int value)
+    { // NOTE: assumes user knows that index starts at zero
+        if (!head)
+            return; // Empty list
+
+        Node *temp = head;
+
+        for (int i = 0; temp, i < value; i++) // traverses through linked list from start if valid, based on value
+            temp = temp->next;                // traverse until found
+
+        if (!temp)
+            return; // Value not found
+
+        temp->prev->next = temp->next;
+        tail = temp->prev; // Deleting the tail
+        
+        delete temp;
+    }
+
+    void delete_node(int value)
+    {
+        if (!head)
+            return; // Empty list
+
+        Node *temp = head;
         while (temp && temp->data != value)
             temp = temp->next;
 
-        if (!temp) return; // Value not found
+        if (!temp)
+            return; // Value not found
 
-        if (temp->prev) {
+        if (temp->prev)
+        {
             temp->prev->next = temp->next;
-        } else {
+        }
+        else
+        {
             head = temp->next; // Deleting the head
         }
 
-        if (temp->next) {
+        if (temp->next)
+        {
             temp->next->prev = temp->prev;
-        } else {
+        }
+        else
+        {
             tail = temp->prev; // Deleting the tail
         }
 
         delete temp;
     }
 
-    void print() {
-        Node* current = head;
-        if (!current) return;
-        while (current) {
+    void print()
+    {
+        Node *current = head;
+        if (!current)
+            return;
+        while (current)
+        {
             cout << current->data << " ";
             current = current->next;
         }
         cout << endl;
     }
 
-    void print_reverse() {
-        Node* current = tail;
-        if (!current) return;
-        while (current) {
+    void print_reverse()
+    {
+        Node *current = tail;
+        if (!current)
+            return;
+        while (current)
+        {
             cout << current->data << " ";
             current = current->prev;
         }
         cout << endl;
     }
 
-    //destructor
-    ~DoublyLinkedList() {
-        while (head) {
-            Node* temp = head;
+    // destructor
+    ~DoublyLinkedList()
+    {
+        while (head)
+        {
+            Node *temp = head;
             head = head->next;
             delete temp;
         }
@@ -158,12 +235,13 @@ public:
 };
 
 // Driver program
-int main() {
+int main()
+{
     DoublyLinkedList list;
-    int size = rand() % (MAX_LS-MIN_LS+1) + MIN_LS;
+    int size = rand() % (MAX_LS - MIN_LS + 1) + MIN_LS;
 
     for (int i = 0; i < size; ++i)
-        list.push_back(rand() % (MAX_NR-MIN_NR+1) + MIN_NR);
+        list.push_back(rand() % (MAX_NR - MIN_NR + 1) + MIN_NR);
     cout << "List forward: ";
     list.print();
 
@@ -176,10 +254,23 @@ int main() {
     // list.print();
 
     cout << "test---------------------\n";
-    list.delete_pos(0); 
     list.print();
 
-    list.delete_pos();
+    list.delete_pos(0); // deletes head based on position
+    list.print();
+
+    list.delete_pos(size-1); // deletes tail based on listSize, note 1 less than variable used to declare because starts at 0. 
+    list.print();
+
+    cout << "Head deletion loop"
+    for (int i = 0; i < 10; i++) // demonstrate head deletion with recursive loop
+    {
+        list.delete_pos(0);
+        list.print();
+    }
+
+    cout << "attempting to pop front\n";
+    list.pop_front();
     list.print();
 
     return 0;
